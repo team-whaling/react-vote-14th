c8967bae361785ee66d9a63a3778847ff9e7b4e8
TaeeunKim <xodms0309@naver.com>
2021-12-05 12:18:04 +0900

    [WHAL-12] style: add buttons

--- src/pages/vote/Vote.tsx
+++ src/pages/vote/Vote.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import useVote from '../../hooks/useVote';
+import styled from 'styled-components';
+import { Col, Row } from '../../components/Containers';
+import { Button } from 'antd';
 const Vote = () => {
   const { candidates } = useVote();
   return (
-    <div>
+    <Container>
       {candidates.map((candidate) => {
         return (
-          <div key={candidate.id}>
-            {candidate.name} {candidate.vote}
-          </div>
+          <VoteContainer>
+            <div key={candidate.id}>
+              {candidate.name} {candidate.vote}
+            </div>
+            <Button>투표하기</Button>
+          </VoteContainer>
         );
       })}
-    </div>
+    </Container>
   );
 };
+const Container = styled(Col)`
+  justify-content: center;
+  align-items: center;
 
+  height: 100vh;
+
+  margin: 0 10%;
+`;
+const VoteContainer = styled(Row)`
+  align-items: center;
+
+  margin: 10px;
+`;
 export default Vote;
