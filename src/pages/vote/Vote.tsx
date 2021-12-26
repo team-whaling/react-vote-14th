ccb43146f5e62e06433df22c45649ec7d489a3c3
TaeeunKim <xodms0309@naver.com>
2021-12-26 14:23:20 +0900

    refactor: delete unused conde

--- src/pages/vote/Vote.tsx
+++ src/pages/vote/Vote.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import useVote from '../../hooks/useVote';
 import styled from 'styled-components';
-import { Col, Row } from '../../components/Containers';
+import { Col } from '../../components/Containers';
 import { Button, Table } from 'antd';
 import useAuth from '../../hooks/useAuth';
 const Vote = () => {
